5e4d33cdbe61f9ff5ff503e5fa31b90fdb7912e3
pradipta <pradiptananda.dalai@gmail.com>
2021-03-22 12:07:41 +0530

    second commit

--- src/main/java/com/arris/crudOperation/controller/CrudOperationController.java
+++ src/main/java/com/arris/crudOperation/controller/CrudOperationController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ParseException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.arris.crudOperation.daoimpl.CrudOperationDaoImpl;
 import com.arris.crudOperation.pojo.CrudOperationPojo;
-import com.sun.el.parser.ParseException;
 
 @RestController
 public class CrudOperationController {
--- src/test/java/com/arris/crudOperation/CrudOperationApplicationTests.java
+++ src/test/java/com/arris/crudOperation/CrudOperationApplicationTests.java
@@ -1,6 +1,6 @@
 package com.arris.crudOperation;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
